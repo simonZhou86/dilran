8bcc63adb2d091fbbf003548b2ae26c58656786b
simonZhou86 <simon.zhou@queensu.ca>
2022-11-18 00:00:06 -0500

    Update our_utils.py

--- our_utils.py
+++ our_utils.py
@@ -5,6 +5,7 @@
 '''
 Change log: 
 - Simon: file created, implement edge detector
+- Simon: create helper function for perceptual loss
 '''
 
 import torch
@@ -62,5 +63,43 @@ class percep_loss(nn.Module):
     '''
     compute perceptual loss between fused image and input image
     '''
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, block_idx, device):
+        '''
+        block_index: the index of the block in VGG16 network, int or list
+        int represents single layer perceptual loss
+        list represents multiple layers perceptual loss
+        '''
+        super(percep_loss).__init__()
+        self.block_idx = block_idx
+        self.device = device
+        # load vgg16_bn model features
+        self.vgg = vgg16_bn(pretrained=True).features.to(device).eval()
+        self.loss = nn.MSELoss()
+
+        # unable gradient update
+        for param in self.vgg.parameters():
+            param.requires_grad = False
+        
+        # remove maxpooling layer
+        bns = [i - 2 for i, m in enumerate(self.vgg) if isinstance(m, nn.MaxPool2d)]
+
+        # register forward hook
+        self.hooks = [PercepHook(self.vgg[bns[i]]) for i in block_idx]
+        self.features = self.vgg[0: bns[block_idx[-1]] + 1]
+
+    def forward(self, inputs, targets):
+        '''
+        compute perceptual loss between inputs and targets
+        '''
+        self.features(inputs)
+        input_features = [hook.features.clone() for hook in self.hooks]
+
+        self.features(targets)
+        target_features = [hook.features for hook in self.hooks]
+
+        assert len(input_features) == len(target_features), 'number of input features and target features should be the same'
+        loss = 0
+        for i in range(len(input_features)):
+            loss += self.loss(input_features[i], target_features[i]) # mse loss
+        
+        return loss
